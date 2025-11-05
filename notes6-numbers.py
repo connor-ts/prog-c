315e98e3fd65e53414335968c514a87c85b92030
connor-ts <connorterstege2@gmail.com>
2025-11-05 13:05:55 -0800

    instructions

---
+++ notes6-numbers.py
@@ -0,0 +1,21 @@
+# operations and numbers 
+# connor ter stege 
+# 5 nov 25 
+
+# Version 1
+# create an algorithm to gather data to find the most popular bubble tea place around us
+# choices are: 
+# ask the user for their favorite bubble tea place
+# store the responses in a list
+# give a raw score for each place
+# give scores as percentages based on the number of responses for each place
+
+
+
+# Version 2
+# ask the user what their favorite bubble tea place is
+# add their response to a list
+# give raw scores for each place
+# gve scores as percentages based on the number of responses for each place
+
+
