5a40e0ed9028d098928059aa03d144d10180a514
connor-ts <connorterstege2@gmail.com>
2025-12-15 11:23:43 -0800

    idk

---
+++ work-dataanalysis.py
@@ -0,0 +1,13 @@
+# Data Analysis
+# connor ter stege
+# dec 9
+
+# Analyse the data provided in class
+
+def main():
+    # do your work in here
+    # or create a new function and call it in here
+    pass
+
+if __name__ == "__main__":
+    main()
