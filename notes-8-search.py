4bb11a21e8a3de251355675480b00bb24c46db46
connor <connorterstege2@gmail.com>
2026-01-13 10:09:48 -0800

    gulp
    Implemented linear and binary search functions with example usage.

--- notes-8-search.py
+++ notes-8-search.py
@@ -1,11 +1,43 @@
 # Intro to Search
+# these my own notes bc i forgor
 # connor ter stege
 # dec 9
 
 # Introduction to search algorithms
 
-def main():
-    pass
+# linear search
+# should work on any kind of list
 
-if __name__ == "__main__":
-    main()
+def linear(data, target):
+    for item in data 
+        if item == target
+        return True
+    return False
+
+numbers = [x, y, z, a, b ,c]
+print(linear(a)) # True
+print(linear(pineapple)) # False
+
+# binary search, althoiugh idk if u did this
+# works on sorted lists
+
+def binary(data, target):
+    low = 0
+    high = len(data) - 1
+
+while low <= high:
+    mid = (low + high) // 2
+
+    if data mid == target
+        return True
+    elif data[mid] < target:
+        low - mid + 1
+    else: high = mid - 1
+
+return False
+
+numbers = [a, b, c, d, e]
+
+print(binary(chocolate_chips)) # False
+print(binary(e)) # True
+
