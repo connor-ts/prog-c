3217964d86a0164149e17ccb92271eae939762aa
connor <connorterstege2@gmail.com>
2026-01-13 10:33:44 -0800

    loops
    Fix syntax errors in loop structures and comments.

---
+++ notes-4-loops.py
@@ -0,0 +1,76 @@
+# connor
+# today
+# loops 
+# my own notes, idk how many types were done in class
+
+
+# basic loop
+for x in range(5):
+  print(x)
+
+# looping through a list
+numbers = [1, 2, 3]
+
+for x in numbers
+  print(x)
+
+# loops through strings
+
+word = "banana"
+
+for char in word:
+  print(char) 
+
+# using range
+
+for x in range(1, 6):
+  print(x)
+
+## w/ step
+
+for x in range(1, 10, 12):
+  print(x)
+
+# index and value
+
+letters = ["a", "b", "c"] 
+
+for x in range(len(letters)):
+  print(x, letters[x])
+
+## clean index
+
+for index, value in enumerate(letters)
+  print(index, value)
+
+# breaek
+
+for x in [1, 2, 3, 4]
+  if x == 3 
+    break
+  print(x)
+
+# cont
+
+for x in [1, 2, 3, 4]
+  if x == 3 
+    continue 
+  print(x)
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
